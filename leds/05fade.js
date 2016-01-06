e7b956d27dfa090f6b6502d80bbf136720933ace
Jesse Cogollo <cogollo87@gmail.com>
2016-01-06 00:11:08 -0500

    add file to handle led with method fade

---
+++ leds/05fade.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var five = require('johnny-five')
+var board = new five.Board()
+
+board.on('ready', function() {
+  var led = new five.Led(11)
+  led.fadeIn()
+  this.wait(5000, function() {
+    led.fadeOut()
+  })
+})
